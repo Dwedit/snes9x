e2434a758d6a6e61575117d63ed20b77ae8d4218
Dwedit <danweiss@gmail.com>
2018-08-25 14:41:14 -0500

    Restore debug check to find struct variables by file offset

--- snapshot.cpp
+++ snapshot.cpp
@@ -2034,6 +2034,12 @@ static void FreezeStruct (STREAM stream, const char *name, void *base, FreezeDat
 		if (SNAPSHOT_VERSION >= fields[i].deleted_in || SNAPSHOT_VERSION < fields[i].debuted_in)
 			continue;
 
+		if ((((memStream*)stream)->pos() + 11 + (ptr - block)) >= 0xaa7)
+		{
+			int dummy = 0;
+		}
+
+
 		addr = (uint8 *) base + fields[i].offset;
 
 		// determine real address of indirect-type fields
